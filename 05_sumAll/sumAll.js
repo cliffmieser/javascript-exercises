1a961164cae6e067d36b00e47a63b4e6fc9915c9
cliffmieser <cliffmieser@gmail.com>
2022-05-30 19:52:36 -0400

    all test passed in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+const sumAll = function(a, b){
+    // creates an array from start 'a', to end 'b'
+    // adds up all numbers from a to b
+    // returns the result of the sum
+    let arrayOfNums = [];
+    let sumOfNums = 0;
+
+    // if (typeof a !== Number || typeof b !== Number){
+    //     return sumOfNums = "Error";
+    // }
+
+    if ((a < 0 || b < 0) || (typeof a !== 'number' || typeof b !== 'number')){
+        return sumOfNums = "ERROR";
+    } else if (a > b){ //If a is greater than b
+        for (let i = a; i>= b; i--){
+            arrayOfNums.push(i);
+        }
+        //gets sum of numbers in array
+        for (let i in arrayOfNums){
+        sumOfNums += arrayOfNums[i];
+    }
+        return sumOfNums;
+    } else if (a < b) { //If a is less than b
+        for (let i = a; i <= b; i++){
+            arrayOfNums.push(i);
+        }
+        //gets sum of numbers in array
+        for (let i in arrayOfNums){
+            sumOfNums += arrayOfNums[i];
+        }
+
+        return sumOfNums;
+    }   
 
 };
+
+let result = sumAll(10, [1, 3, 5]);
+console.log(result);
+
 
 // Do not edit below this line
 module.exports = sumAll;
