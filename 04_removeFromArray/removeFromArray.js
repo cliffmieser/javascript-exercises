0c58b9ec8b2a56d09e752d41d909f752f6d61925
cliffmieser <cliffmieser@gmail.com>
2022-05-29 22:04:53 -0400

    Array.from() method bug

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,29 @@
-const removeFromArray = function() {
-    let storedArray = arguments[0];
-    let valueToRemove = arguments[1];
+const removeFromArray = function(arg1, ...otherArgs) {
+    // Takes an array and some other arguments
+    // Removes other arguments from the given array
+    // let storedArray = Array.from(arguments[0]);
+    let storedArray = arg1;
+    let valuesToRemove = otherArgs;
+    console.log(valuesToRemove);
     let newArray = [];
     for (let i = 0; i < storedArray.length; i++ ){
-        if (storedArray[i] != valueToRemove){
+        if (storedArray[i] != valuesToRemove){
             newArray.push(storedArray[i]);
+        } else{
+            continue;
         }
     }
     return newArray;
 
-
 };
 
+let NewArray = removeFromArray([1, 2, 3, 4], 3, 2);
+console.log(NewArray);
+
 //First test passed
+// Turning the arguments into a real array:
+// let storedArray = Array.from(arguments[0]);
+// let valuesToRemove = arguments[1];
 
 
 // Do not edit below this line
