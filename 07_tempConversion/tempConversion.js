e3ae3381b4068e24714905689c1ea473f9210e10
cliffmieser <cliffmieser@gmail.com>
2022-05-31 07:53:13 -0400

    All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,27 @@
-const ftoc = function() {
+const ftoc = function(deg) {
+  //takes deg and converts to Celsius
+  let convertedDeg = (deg - 32) * (5/9);
+  let rounded = Math.round(convertedDeg * 10) / 10;
+  return rounded;
+
 
 };
 
-const ctof = function() {
+const ctof = function(deg) {
+  //takes deg and converts to Fahrenheit
+  let convertedDeg =  (deg * (9/5)) + 32;
+  let rounded = Math.round(convertedDeg * 10) / 10;
+  return rounded;
 
 };
+
+
+
+// let result = ftoc(32);
+// console.log(result);
+
+// result = ctof(0);
+// console.log(result);
 
 // Do not edit below this line
 module.exports = {
